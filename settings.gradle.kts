abf1718f8b3c06072e02a823663d3ebab9eccd79
Sam Taiwo <staiwo@onlydates.fan>
2024-12-24 21:38:42 +0000

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+rootProject.name = "taskmanager"
+rootProject.buildFileName = "engine.kotlingen.taskmanager.gradle.kts"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
---
+++ src/commonMain/kotlin/ship/f/engine/kotlingen/taskmanager/TaskManager.kt
@@ -0,0 +1,5 @@
+package ship.f.engine.kotlingen.taskmanager
+
+object TaskManager {
+
+}
